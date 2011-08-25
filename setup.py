d67f7e7daf61889327bd98ecb3496b5c87b29f84
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-25 23:18:02 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='user_profile',
+    version='1.0.1',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_user_profile',
+    packages=['user_profile'],
+)
