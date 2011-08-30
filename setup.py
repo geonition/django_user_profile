a46ba1b368f8fcaed88f74cafa71acf43b7f2656
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-30 16:33:20 +0300

    modified setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='user_profile',
-    version='1.0.1',
+    version='1.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_user_profile',
-    packages=['user_profile'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        "user_profile": [
+            "templates/*.js"
+        ],
+    },
+    zip_safe=False,
+    install_requires=['django']
 )
